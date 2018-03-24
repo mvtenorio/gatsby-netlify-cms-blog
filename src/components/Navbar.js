99a90c826f6567ecbf43cea19cddfbf61f8d1376
Marcus Tenório <marcusvtenorio@gmail.com>
2018-03-24 16:47:11 -0300

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -25,7 +25,7 @@ const Navbar = () => (
       <div className="navbar-end">
         <a
           className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
+          href="https://github.com/mvtenorio/gatsby-netlify-cms-blog"
           target="_blank"
           rel="noopener noreferrer"
         >
